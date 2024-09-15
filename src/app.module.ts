3a26d960c6b1b4095ceb04c7a5a5991cfe50c0d9
Alexandre Hartmann <alexandre.hartmann@o2obots.com>
2024-09-15 15:09:25 -0300

    chore: typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 import { BatidasModule } from './domain/batidas/batidas.module';
 
 @Module({
-  imports: [BatidasModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    BatidasModule,
+  ],
   controllers: [],
   providers: [],
 })
